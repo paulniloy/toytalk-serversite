16e76343e86a20aeb5be85c3e2c4d8cc1c89ff32
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-18 15:58:55 +0600

    language tab done

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+    app.get("/language/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = await usersubcollection.findOne(query);
+      res.send(cursor);
+    })
+
      
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
