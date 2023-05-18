09736cc8a45f03344eb739cbba1d39ef0c7bd316
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-18 13:18:11 +0600

    mongodb install and pinnged successfully

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,6 +6,37 @@ var cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
+
+// niloyp10
+// g9pzZNvk3sJVXcAP
+
+const uri = "mongodb+srv://niloyp10:g9pzZNvk3sJVXcAP@paulniloy.38wqfao.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
